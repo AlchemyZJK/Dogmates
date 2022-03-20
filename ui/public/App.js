beb60027b172065c4901859eef6938af466de3a6
Alchemy <alchemyzjk@gmail.com>
2022-03-20 21:25:14 +0800

    add api package.json

---
+++ ui/public/App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const element = /*#__PURE__*/React.createElement("h1", null, "Dogmates");
+ReactDOM.render(element, document.getElementById('contents'));
